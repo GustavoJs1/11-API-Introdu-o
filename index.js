dc227c643323b03dbf8ae0827367f01483bfbf91
GustavoJs1 <133232641+GustavoJs1@users.noreply.github.com>
2023-08-11 12:13:36 +0000

    Terminei

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import { selectUsuarios, selectUsuario } from "./bd.js";
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./bd.js";
 import express from "express";
-
 dotenv.config();
 
 const app = express();
@@ -25,6 +24,17 @@ app.get("/usuario/:id", async (req, res) => {
   }
 
 });
+app.use(express.json());
+
+app.post("/usuario", async (req, res) => {
+  console.log("Rota POST /usuario solicitada");
+  try {
+    await insertUsuario(req.body);
+    res.status(201).json({ message: "Usuário inserido com sucesso!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 app.get("/usuarios", async (req, res) => {
   try {
     const usuarios = await selectUsuarios();
@@ -34,6 +44,32 @@ app.get("/usuarios", async (req, res) => {
   }
 
   console.log("Rota GET/usuarios solicitada");
+});
+app.delete("/usuario/:id", async (req, res) => {
+  console.log("Rota DELETE /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) {
+      await deleteUsuario(req.params.id);
+      res.status(200).json({ message: "Usuário excluido com sucesso!!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+//index.js
+app.patch("/usuario", async (req, res) => {
+  console.log("Rota PATCH /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.body.id);
+    if (usuario.length > 0) {
+      await updateUsuario(req.body);
+      res.status(200).json({ message: "Usuário atualizado com sucesso!" });
+    } else res.status(404).json({ message: "Usuário não encontrado!" });
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {
