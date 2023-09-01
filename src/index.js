4c7728e46899673e421bce445f93634bfaa40de9
GustavoJs1 <133232641+GustavoJs1@users.noreply.github.com>
2023-09-01 10:51:35 +0000

    teste

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import dotenv from "dotenv";
 import express from "express";
 dotenv.config();
 
-app.use(roteadorUsuario);
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+
+app.use(roteadorUsuario);
 
 app.get("/", (req, res) => {
   res.json({
